659bf9a629476822876b58942ee56654a3839482
Anurat Eiamphoklarp <rhymerealm@gmail.com>
2024-01-29 16:13:16 +0700

    v0.3.3

--- app.js
+++ app.js
@@ -14,6 +14,52 @@ const $keys = [
 ];
 
 
+function getData(dataText) {
+  
+  let data;
+  const dataJSON = dataText || localStorage.getItem('c60-cms') || '[]';
+  
+  // Fix corrupted data.
+  try {
+    data = JSON.parse(dataJSON);
+  } catch(e) {
+    data = [];
+    localStorage.setItem('c60-cms', '[]');
+  }    
+  
+  // Fix data container is not array.
+  if (data.constructor.name !== 'Array') {
+    data = [data];
+    localStorage.setItem('c60-cms', JSON.stringify(data));
+  }
+  
+  // Convert old data structure 2D array to object array.
+  if (Array.isArray(data[0])) {
+    
+    const keys = [
+      'หมวด', 'ส่วน', 'มาตรา', 'ร่างมาตรา',
+      'ร่างบทบัญญัติ', 'ประเด็นการพิจารณา', 'มติที่ประชุม',
+      'หมายเหตุ', 'ผู้อภิปราย', 'ประชุมครั้งที่', 'หน้า'
+    ];
+
+    const newData = data
+        .map(row => row.toSpliced(10, 1))
+        .map(row => {
+            const newRow = {};
+            row.forEach((col, index) => newRow[keys[index]] = col)
+            return newRow;
+        });
+
+    const newJSON = JSON.stringify(newData);
+    localStorage.setItem('c60-cms', newJSON);
+    return newData;
+    
+  }
+  return data;
+}
+
+
+
 function render({ id, to, remove }) {
   const isNumber = param => Number.isInteger(param);
   if (isNumber(id)) {
@@ -598,7 +644,7 @@ function upload(evt) {
     reader.addEventListener('load', evt => {
       const result = evt.target.result;
       try {
-        const json = JSON.parse(result);
+        const json = getData(result);
         $data = json;
         save($data);
         render({});
